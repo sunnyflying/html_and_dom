aab8d59bddd87c5b2bd18cca2a3a695ea961c318
mkyang <303055758@qq.com>
2014-12-16 13:53:40 +0800

    clear blank line

--- examOnLine.js
+++ examOnLine.js
@@ -2,15 +2,20 @@ function getGrade() {
   var total = 0;
   var corret = ["封装性","继承性","多态性"];
   judgeNull();
+
   var totalSelete = getSelete("selete1", "b") + getSelete("selete2", "c");
+
   var totalDuoSelete = getDuoSelete("duoselete1", "a,b,d") + getDuoSelete("duoselete2", "a,b,c");
+
   var totalInput = getInput1("UML", "统一建模语言") +
                    getInput2("object1", corret) +
                    getInput2("object2", corret) +
                    getInput2("object3", corret);
 
   var totalText = getText();
+
   var totalJudge = getJudge("judge1", "false") + getJudge("judge2", "true");
+
   total = totalSelete + totalDuoSelete + totalInput + totalText + totalJudge;
   document.getElementById("print").innerHTML=total;
 }
@@ -28,6 +33,7 @@ function judgeNull() {
 function getJudge(judge, anwser) {
   var total = 0;
   var judgeAnwser = document.getElementsByName(judge);
+
   for (var i = 0; i < judgeAnwser.length; i++) {
     if (judgeAnwser[i].checked === true && judgeAnwser[i].value === anwser) {
       total = 10;
@@ -58,7 +64,7 @@ function getDuoSelete(duoSelete, anwser) {
       array.push(duoSeleteAnwser[i].value);
     }
   }
-  var a = array.toString();
+
   if (array.toString() === anwser) {
     total = 10;
   }
@@ -80,6 +86,7 @@ function getInput1(input, anwser) {
 function getInput2(input, corret) {
   var total = 0;
   var inputAnwser = document.getElementById(input).value;
+
   for (var i = 0; i < corret.length; i++) {
     if (inputAnwser === corret[i]) {
       total += 5;
@@ -90,8 +97,9 @@ function getInput2(input, corret) {
 
 function getText() {
   var total = 0;
-  var input1 = document.getElementById("ask").value;
-  if (input1 === "模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。") {
+  var input = document.getElementById("ask").value;
+
+  if (input === "模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。") {
     total = 20;
   }
   return total;
